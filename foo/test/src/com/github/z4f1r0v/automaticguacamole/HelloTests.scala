ece18d97b12f1df86c34bac45f27e09199b3b620
Alexander Zafirov <alexander@zafirov.me>
2020-05-25 15:21:43 +0200

    Add utest

---
+++ foo/test/src/com/github/z4f1r0v/automaticguacamole/HelloTests.scala
@@ -0,0 +1,11 @@
+package com.github.z4f1r0v.automaticguacamole
+
+import utest.{TestSuite, Tests, test}
+
+object HelloTests extends TestSuite {
+  val tests = Tests {
+    test("test2") {
+      1
+    }
+  }
+}
